19193a284b47a44d7d08da7039879a2936295419
Ryastan <97860651+Ryastan@users.noreply.github.com>
2022-03-16 11:50:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from discord.ext import commands
 import json
 import requests
 
-TOKEN = "OTUxOTg5Nzg1OTk0NjEyNzQ3.YivflA.I-_BXijOvOLraOcN9qsxUyYbbqQ"
+TOKEN = ""
 
 
 class Bot_commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command(name='meme')
+    @commands.command(name='meme') #Команда с рандомными мемами
     async def meme(self, ctx):
-        URL = "https://api.humorapi.com/memes/random"
+        URL = "https://api.humorapi.com/memes/random" #URL randomMeMe
         params = {
             'api-key': "59bf2775d7c244aeac828e67f4f05ca0"
         }
         response = requests.get(URL, params).json()
-        await ctx.channel.send(response['url'])
+        await ctx.channel.send(response['url']) #Вывод ссылки(картинки) с мемом
 
 
 bot = commands.Bot(command_prefix='!')
