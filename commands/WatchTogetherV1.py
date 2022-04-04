830c07f038730e1faab4b54504aa9be3ddbae410
Ryastan <97860651+Ryastan@users.noreply.github.com>
2022-04-04 22:16:56 +0300

    хз API W2G не работает

--- commands/WatchTogetherV1.py
+++ commands/WatchTogetherV1.py
@@ -11,15 +11,15 @@ class WT_command(commands.Cog):
     @commands.command(name='watch') #Команда с рандомными мемами
     async def create_WT(self, ctx, url):
         params = {
-            'w2g_api_key': "<9h4xsjcz3qgrn0arl01jrdqccdtf2a8ad6c39jh1ujiipt7jfsyqvozrpnr9o6k9>",
+            'w2g_api_key': "9h4xsjcz3qgrn0arl01jrdqccdtf2a8ad6c39jh1ujiipt7jfsyqvozrpnr9o6k9",
             'share': url,
             'bg_color': '#00ff00',
-            'bp_opacity': '50'
-        }
-        headers = {
+            'bp_opacity': '50',
+            'method': 'POST',
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-            }
-        response = requests.get('https://w2g.tv/rooms/create.json', params=params, headers=headers).json()
+        }
+        response = requests.get('https://w2g.tv/rooms/create.json', params=params).json()
         with open("response_WT.json", mode='w', encoding='utf-8')as file:
             json.dump(response, file)
+        
