8670b276e6d10105f451b54ef33d917e772cb38a
Borov <aladzhev_max@mail.ru>
2022-04-26 23:14:50 +0300

    Небольшие правки и начало работы над функцией репутации

--- commands/reputation.py
+++ commands/reputation.py
@@ -1,4 +1,5 @@
 import discord
+import json
 from discord.ext import commands
 import json
 import requests
@@ -11,4 +12,12 @@ class Reputation_command(commands.Cog):
 
     @commands.command(name='myrep')  # Команда с репутацие пользоваеля
     async def meme(self, ctx):
-        await ctx.channel.send("Пытаюсь сделать работу с базой в txt файле")
+        with open('reputation.json') as rep_file:
+            data = json.load(rep_file)
+        if str(ctx.author.bot) in rep_file:
+            rep_file[str(ctx.author.bot)] += 1
+        else:
+            rep_file[str(ctx.author.bot)] = 1
+        await ctx.channel.send("Ваша репутация теперь равна", str(rep_file[str(ctx.author.bot)]))
+        with open('reputation.json', 'w') as cat_file:
+            json.dump(rep_file, cat_file)
